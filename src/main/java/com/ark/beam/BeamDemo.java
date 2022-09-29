9e56e8ef8279e624ddafdd71d69f026ccdd8ed8a
abdulrabkhan01 <abdul.lambent@gmail.com>
2022-09-29 23:13:18 +0530

    First example

--- src/main/java/com/ark/beam/BeamDemo.java
+++ src/main/java/com/ark/beam/BeamDemo.java
@@ -1,9 +1,16 @@
 package com.ark.beam;
 
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+
 public class BeamDemo {
 
     public static void main(String[] args) {
-
+        System.out.println("Demo of pipe line options");
+        PipelineOptions pipelineOptions = PipelineOptionsFactory.create();
+        System.out.println("Job Name "+pipelineOptions.getJobName());
+        System.out.println("Id "+pipelineOptions.getOptionsId());
+        System.out.println("Temp Location "+pipelineOptions.getTempLocation());
     }
 
 }
